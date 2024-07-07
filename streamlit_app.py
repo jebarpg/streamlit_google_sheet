bffe3df1ce8a7a4e006f2adcfb2e638d9b13bc41
Jeb <130810521+jebarpg@users.noreply.github.com>
2024-07-07 00:48:54 +0000

    removed text

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,17 +8,3 @@ url = "https://docs.google.com/spreadsheets/d/1pZkq7l2JPEmIePg8acAD_rPM0FHXwp6p5
 conn = st.connection("gsheets", type=GSheetsConnection)
 data = conn.read(spreadsheet=url)
 st.dataframe(data)
-
-# Introduction text
-"""
-Hi there! This is a simple Google Sheets example on how to embed a dynamic table on a Medium blog post with Streamlit! 
-"""
-
-
-# Footer text
-"""
-The data shown above is the historic stock price of Google. In the "Graph" tab I have put an example of a simple graph that can be embedded. 
-"""
-"""
-You can read my blog here: https://medium.com/@steffenjanbrouwer/how-to-embed-data-tables-and-graphs-in-your-medium-blogs-16d93d99ebc7
-"""
